0bf9fe4fa71d7a651863ee72ca490aa180b0ce0b
vinay-nethala <155607912+vinay-nethala@users.noreply.github.com>
2026-01-08 12:52:19 +0530

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,32 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts/contracts",
+    tests: "./contracts/test",
+    cache: "./contracts/cache",
+    artifacts: "./contracts/artifacts"
+  },
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+    hardhat: {
+      chainId: 31337
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
